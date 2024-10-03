85a3d03abf46a446494583b1d3216987089fdbbf
bigbowltakestime <bigbowltakestime@gmail.com>
2024-10-04 03:39:08 +0900

    [init] 기본 셋팅

---
+++ src/common/user/user.entity.ts
@@ -0,0 +1,11 @@
+// src/user/user.entity.ts
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+}
---
+++ src/common/user/user.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findOne(id: number): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+}
